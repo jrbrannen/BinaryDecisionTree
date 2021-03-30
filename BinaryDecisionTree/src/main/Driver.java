f1402a0247a6019d5d1b1d02878efbdef7f59d5e
jrbrannen <71732832+jrbrannen@users.noreply.github.com>
2021-03-29 22:49:28 -0500

    Initial Commit
    Use a binary tree for making decisions

---
+++ BinaryDecisionTree/src/main/Driver.java
@@ -0,0 +1,37 @@
+/**************************************************************
+* Name        : Binary Decision Tree
+* Author      : Jeremy Brannen
+* Created     : 3/29/2021
+* Course      : CIS 152 Data Structures
+* Version     : 1.0
+* OS          : Windows 10
+* Copyright   : This is my own original work based on
+*               specifications issued by our instructor
+* Description : This program overall description here
+*               Input:  list and describe
+*               Output: list and describe
+* Academic Honesty: I attest that this is my original work.
+* I have not used unauthorized source code, either modified or 
+* unmodified. I have not given other fellow student(s) access to
+* my program.         
+***************************************************************/
+
+package main;
+import model.Question;
+
+/**
+ * @author Jeremy Brannen - jrbrannen
+ *CIS175
+ * Mar 29, 2021
+ */
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		Question.response();
+		
+	}	
+	
+
+}
